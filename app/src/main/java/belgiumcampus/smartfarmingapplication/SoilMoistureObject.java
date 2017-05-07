457c526a6944a1e263875e38eb04edda57a3cf6b
Pieter <pieterventer571@gmail.com>
2017-05-07 15:49:44 +0100

    soil moisture KLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR

--- app/src/main/java/belgiumcampus/smartfarmingapplication/SoilMoistureObject.java
+++ app/src/main/java/belgiumcampus/smartfarmingapplication/SoilMoistureObject.java
@@ -8,18 +8,42 @@ import android.content.Context;
 
 public class SoilMoistureObject {
 
-    public static String OverallMoistureReading(Context context)
+    public static String MoisturePercentage(Context context)
     {
 
 
         String receivedData = "";
 
         //procedure name           //columns  //rows //specific column
-        receivedData =  DataAccess.readData(context,"sp_ShowLatestWaterTableValue",  "1"   ,    "1",      "1");
+        receivedData =  DataAccess.readData(context,"sp_soilmoisture",  "1"   ,    "1",      "1");
         return receivedData.replace(";","").trim();
 
 
     }
 
+    public static String Recommendation(Context context)
+    {
+
+
+        String receivedData = "";
+
+        //procedure name           //columns  //rows //specific column
+        receivedData =  DataAccess.readData(context,"sp_soilmoisture",  "2"   ,    "1",      "2");
+        return receivedData.replace(";","").trim();
+
+
+    }
+    public static String Date(Context context)
+    {
+
+
+        String receivedData = "";
+
+        //procedure name           //columns  //rows //specific column
+        receivedData =  DataAccess.readData(context,"sp_soilmoisture",  "3"   ,    "1",      "3");
+        return receivedData.replace(";","").trim();
+
+
+    }
 
 }
